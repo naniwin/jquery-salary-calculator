92a61f604dbe71ca99d23d4eb38a8c24af516a9f
Jonathan Nguyen <njonathan990@gmail.com>
2022-04-15 15:27:30 -0500

    code to display on table, but not next row

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -18,15 +18,39 @@ function addEmployee() {
   employee.push(newEmployee);
   console.log(employee);
   displayEmployee();
+  emptyEmployeeInput();
 } // end addEmployee
 
 function displayEmployee() {
   console.log('in displayEmployee');
-  let el = $('#employeesOut');
+  let el = $('.employeesOut');
+  let lineNo = 1;
   el.empty();
   for (let i = 0; i < employee.length; i++) {
     el.append(
-      `<li>${employee[i].first} ${employee[i].last} ${employee[i].id} ${employee[i].title} $${employee[i].salary} <button class="deleteEmployeeButton" data-index="${i}">Delete</button></li>`
-    );
+      '<td>' +
+        employee[i].first +
+        '</td> <td>' +
+        employee[i].last +
+        '</td> <td>' +
+        employee[i].id +
+        '</td> <td>' +
+        employee[i].title +
+        '</td> <td>' +
+        employee[i].salary
+    ) +
+      '</td>' +
+      '<td><button class="deleteEmployeeButton">Delete</button></td>';
+    //   `<td>${employee[i].first}</td> <td>${employee[i].last}</td> <td>${employee[i].id}</td> <td>${employee[i].title}</td> <td>$${employee[i].salary}</td> <td><button class="deleteEmployeeButton" data-index="${i}">Delete</button></td>`
+    // );
+    lineNo++;
   }
 }
+
+function emptyEmployeeInput() {
+  $('#firstNameIn').val('');
+  $('#lastNameIn').val('');
+  $('#idNumberIn').val('');
+  $('#titleIn').val('');
+  $('#annualSalaryIn').val('');
+} // end empty input value
