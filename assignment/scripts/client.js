26c1ed3589cb6dba43337afe368d6f303e2e2c10
Jonathan Nguyen <njonathan990@gmail.com>
2022-04-15 00:57:14 -0500

    added displayout and add employee function

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,9 +1,32 @@
 $(document).ready(onReady);
 
+let employee = [];
+
 function onReady() {
   $('#addEmployeeButton').on('click', addEmployee);
 }
 
 function addEmployee() {
   console.log('in addEmployee');
+  let newEmployee = {
+    first: $('#firstNameIn').val(),
+    last: $('#lastNameIn').val(),
+    id: $('#idNumberIn').val(),
+    title: $('#titleIn').val(),
+    salary: $('#annualSalaryIn').val(),
+  };
+  employee.push(newEmployee);
+  console.log(employee);
+  displayEmployee();
+} // end addEmployee
+
+function displayEmployee() {
+  console.log('in displayEmployee');
+  let el = $('#employeesOut');
+  el.empty();
+  for (let i = 0; i < employee.length; i++) {
+    el.append(
+      `<li>${employee[i].first} ${employee[i].last} ${employee[i].id} ${employee[i].title} $${employee[i].salary} <button class="deleteEmployeeButton" data-index="${i}">Delete</button></li>`
+    );
+  }
 }
